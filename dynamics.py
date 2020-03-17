ac2a7f86a5814ca24d04b7611cfb28d51a53c4ae
johannes <johannes@pej.se>
2020-03-17 11:00:14 +0100

    More utils

--- dynamics.py
+++ dynamics.py
@@ -8,19 +8,29 @@ def henon(x, a=1.4, b=0.3):
     return np.stack([1-a*x**2+y, b*x], -1)
 
 
+def attractor_points(f, n, k=100, condition=None):
+    if condition is None:
+        condition = lambda x: np.sum(x**2, 1) < 1000
+    x = 5*np.random.randn(n, 2)
+    for i in range(k):
+        x = f(x)
+        x = x[condition(x)]
+    return x[:n, :]
+
+
 def iter(f, x, k):
     """
     f tar en matris av form NxD
     där N är antalet punkter och D är dimensionen av X
     """
-    x = np.array(x).reshape(-1, len(x))
+    x = np.array(x)
     for i in range(k):
         x = f(x)
     return x
 
 
 def itercat(f, x, k):
-    x = np.array(x).reshape(-1, len(x))
+    x = np.array(x)
     xs = [x]
     for i in range(k):
         xs.append(f(xs[-1]))
